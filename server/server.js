d613ba3e8572c001ee1811727f8600e0e8a43367
Vishnu <vishnujangid7413666@gmail.com>
2025-07-19 22:03:14 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -13,15 +13,7 @@ await connectDb();
 // app.use(cors())
 
 
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-      "https://quick-blog-green.vercel.app" // your frontend URL
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    credentials: true,
-    allowedHeaders: ["Content-Type", "Authorization"],
-  })
-);
+app.use(cors());
 
 app.use(express.json())
 
